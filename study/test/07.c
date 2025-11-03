b36ca604f59346118a7b43285ff67417727a6fc2
lsy8658 <socszz@gmail.com>
2025-11-03 21:34:34 +0900

    c

--- study/test/07.c
+++ study/test/07.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+// ----------------------------------
 
-// int doWhile () {
-//     int sum = 0;
-//     do {
-//         sum += 1;
-//         printf("sumÀÇ µ¡¼À %d\n", sum);
-//     } while (sum < 10);
-// }
 
 // ----------------------------------
 
-// int whileTest () {
-//     int sum = 0;
-//     while (sum < 10) {
-//         sum += 1;
-//         printf("sumÀÇ µ¡¼À %d\n", sum);
-//     }
-    
-// }
+int fncMath () {
+  double x = 12.34;
+  int i = -5;
+  int a,b,c,d,e,f;
+  a = (int) ceil(x); // ????
+  b = (int) floor(x); // ????
+  c = (int) pow(4, 2);
 
+  printf("abs(-5) = %d\n", abs(i));
+  printf("ceil => %d\n", a);
+  printf("floor => %d\n", b);
+  printf("cos => %f\n", cos(10));
+  printf("exp => %f\n", exp(2));
+  printf("sqrt => %d\n", sqrt(2));
+  printf("pow => %d\n", c);
+};
 // ----------------------------------
 
-int switchTest () {
+int fncStr () { 
+  // isalpha : ??? ???(A–Z, a–z) ?? ??? ? ??
+  // isdigit : ? ??? ?????
 
 }
 
 // ----------------------------------
 
+// ----------------------------------
+
+// ----------------------------------
+
+// ----------------------------------
+
+// ----------------------------------
 int main () {
-  switchTest();
+ 
 }
 
 // ----------------------------------
